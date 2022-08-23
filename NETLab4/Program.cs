19622f3287f64f1f62c3e12263ed392eaa7e9dac
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-23 17:54:44 +0300

    Update Program.cs

--- NETLab4/Program.cs
+++ NETLab4/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using NETLab4.Parsers;
-using NETLab4.Parts;
+using System;
+using NETLab4.ExpressionEditors;
+using NETLab4.SimpleExpressions;
 using NETLab4.Tree;
 
 namespace NETLab4
@@ -11,21 +11,24 @@ namespace NETLab4
         {
             try
             {       
-                Console.WriteLine("Запишiть ваш вираз у форматi (4+x)/(7*...)-...");
+                Console.WriteLine("Запишiть ваш вираз у форматi:\n" +
+                    "\tвираз = <простий вираз> | <складний вираз>\n" +
+                    "\tпростий вираз = <число> | <iм'я змiнної>\n" +
+                    "\tскладний вираз = (<вираз><знак операцiї><вираз>)\n" +
+                    "Наприклад: ((62+3)/(8,45*7))-4");
                 string? exp = Console.ReadLine();
                 Console.WriteLine("Розбирається задане дерево парсером виразiв. Готово:");
 
                 if (!String.IsNullOrEmpty(exp))
                 {
                     var root = ExpressionParser.Parse(exp);
-                    Console.WriteLine(root.Expression());
+                    Console.WriteLine(ExpressionDisplayer.Display(root));
                 }
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
             Console.ReadLine();
         }
     }
