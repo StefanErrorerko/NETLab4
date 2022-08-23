9889ca2031b226954b28466c4155d7b929e37681
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-23 17:41:16 +0300

    Update ExpressionParser.cs

--- NETLab4/Parsers/ExpressionParser.cs
+++ NETLab4/Parsers/ExpressionParser.cs
@@ -1,26 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using NETLab4.Tree;
-using NETLab4.Parts;
+using NETLab4.SimpleExpressions;
 
-namespace NETLab4.Parsers
+namespace NETLab4.ExpressionEditors
 {
-    public static class ExpressionParser
+    public static class ExpressionParser    //??? норм шо паблік а не інтернал?
     {
+        private static bool part_recording; 
+        private static Component? currentNode; // ??? camel norm, почитати про конвенцію
+        private static string? part_value;  // ??? а шо робить з '?'
+        private static Component? lastNode;
+
         public static Component Parse(string exp)
         {
             var root = new Composite();
-            Component currentNode = root;
+           
+            part_value = String.Empty;
+            currentNode = root;
+            part_recording = false;
 
             foreach (char s in exp)
             {
                 switch (s)
                 {
                     case '(':
-                        var lastNode = currentNode;
+                        lastNode = currentNode;
                         if (currentNode.Left == null)
                         {
                             currentNode.Left = new Composite();
@@ -34,56 +38,68 @@ namespace NETLab4.Parsers
                         currentNode.Parent = lastNode;
                         break;
                     case ')':
+                        RecordValueAndCreateLeaf();
                         if (currentNode.Parent == null) currentNode.Parent = new Composite();
                         currentNode = currentNode.Parent;
-                        break;
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '0':
-                        if (currentNode.Connector == null && currentNode.Left != null)
-                        {
-                            currentNode.Left.Value.Value *= 10;
-                            currentNode.Left.Value.Value += Convert.ToInt32(s.ToString());
-                        }
-                        else if (currentNode.Connector != null && currentNode.Right != null)
-                        {
-                            currentNode.Right.Value.Value *= 10;
-                            currentNode.Right.Value.Value += Convert.ToInt32(s.ToString());
-                        }
-                        else
-                        {
-                            if (currentNode.Left == null) currentNode.Left = new Leaf(
-                                new Number(Convert.ToInt32(s.ToString())));
-                            else currentNode.Right = new Leaf(
-                                new Number(Convert.ToInt32(s.ToString())));
-                        }
                         break;
                     case '*':
                     case '/':
                     case '+':
                     case '-':
-                        while (currentNode.Connector != null)
-                        {
-                            if (currentNode.Parent == null) currentNode.Parent = new Composite();
-                            currentNode = currentNode.Parent;
-                        }
+                        if(part_recording) 
+                            RecordValueAndCreateLeaf();
                         currentNode.Connector = new Connect(s);
                         break;
                     default:
-                        if (currentNode.Left == null) currentNode.Left = new Leaf(new Variable(s));
-                        else currentNode.Right = new Leaf(new Variable(s));
+                        part_recording = true; // ??? чи делегат чи подія
+                        part_value += s;
                         break;
                 }
             }
-            return root;
+            if(part_value != null) 
+                RecordValueAndCreateLeaf();
 
+            return currentNode;
+        }
+
+        private static SimpleExpression PartRecord(string raw_part)
+        {
+            if (IsNumber(raw_part)) return new Number(Convert.ToDouble(raw_part));
+            else if (IsVariable(raw_part)) return new Variable(raw_part);
+            else throw new InvalidDataException();
+        }
+
+        private static bool IsVariable(string raw_part)
+        {
+            return Regex.IsMatch(raw_part, @"^[A-z,a-z]+$");        // ??? чи норм? або розширювати Стрінг? але це крінж чи у Variable
+        }
+
+        private static bool IsNumber(string raw_part)
+        {
+            try
+            {
+                Convert.ToDouble(raw_part);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static void RecordValueAndCreateLeaf()  
+        {
+            if (part_recording)
+            {
+                if (currentNode == null || part_value == null)
+                    throw new NullReferenceException("Recording was not run or current node was not selected");
+                if (currentNode.Left == null)
+                    currentNode.Left = new Leaf(PartRecord(part_value));  
+                else
+                    currentNode.Right = new Leaf(PartRecord(part_value));    
+                part_value = String.Empty;
+                part_recording = false;
+            }
         }
     }
 }
