ab08467939f11a66c1c0adfc9c486dcf9e9c2d24
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-23 17:53:39 +0300

    Update Leaf.cs
    Remove IsExpression()
    Part -> SimpleExpression
    Expression() -> ToString()

--- NETLab4/Tree/Leaf.cs
+++ NETLab4/Tree/Leaf.cs
@@ -2,27 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NETLab4.Parts;
+using NETLab4.SimpleExpressions;
 
 namespace NETLab4.Tree
 {
     public class Leaf : Component
     {
-        public override Part Value { get; set; }
-        public Leaf(Part value)
+        public override SimpleExpression Value { get; set; }
+        public Leaf(SimpleExpression value)
         {
             Value = value;
         }
 
-        public override string Expression()
+        public override string ToString()
         {
             if(Value != null) return Value.ToString();
             return String.Empty;
-        }
-
-        public override bool IsExpression()
-        {
-            return false;
         }
     }
 }
