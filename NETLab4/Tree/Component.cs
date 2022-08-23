a0fec65fa0be8ce20254144d2b0aa9bf56dbfa02
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-23 17:51:07 +0300

    Deleted funcs Expression(), IsExpression()

--- NETLab4/Tree/Component.cs
+++ NETLab4/Tree/Component.cs
@@ -9,12 +9,5 @@ namespace NETLab4.Tree
         public virtual Component Right { get; set; }
         public virtual Component Parent { get; set; }
         public virtual Connect Connector { get; set; }
-
-        public abstract string Expression();
-
-        public virtual bool IsExpression()
-        {
-            return true;
-        }
     }
 }
