ee9df4d0eb3691f800e364369ac6af6e6adae51a
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-26 16:36:11 +0300

    Update null refernece types

--- NETLab4/Tree/Component.cs
+++ NETLab4/Tree/Component.cs
@@ -1,13 +1,13 @@
-﻿using NETLab4.SimpleExpressions;
+using NETLab4.SimpleExpressions;
 
 namespace NETLab4.Tree
 {
     public abstract class Component
     {
         public virtual SimpleExpression Value { get; set; }
-        public virtual Component Left { get; set; }
-        public virtual Component Right { get; set; }
-        public virtual Component Parent { get; set; }
-        public virtual Connect Connector { get; set; }
+        public virtual Component? Left { get; set; }
+        public virtual Component? Right { get; set; }
+        public virtual Component? Parent { get; set; }
+        public virtual Connect? Connector { get; set; }
     }
 }
