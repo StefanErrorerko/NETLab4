3f30540ae0ca58728075f3d690bb97f4a7395596
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-26 16:35:42 +0300

    Update Variable.cs

--- NETLab4/SimpleExpressions/Variable.cs
+++ NETLab4/SimpleExpressions/Variable.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace NETLab4.SimpleExpressions
 {
     public class Variable : SimpleExpression
     {
         public string Name { get; init; }
-        public override double Value { get; set; }
+        public override double Value { get; init; }
 
         public Variable(string name, double value)
         {
@@ -19,6 +16,14 @@ namespace NETLab4.SimpleExpressions
         public Variable(string name)
         {
             Name = name;
+        }
+
+        public static bool TryParse([NotNullWhen(true)] string? str, [NotNullWhen(true)] out Variable? result)
+        {
+            if (str != null && Regex.IsMatch(str, @"^[A-z,a-z]+$"))
+                result = new Variable(str);
+            else result = null;
+            return result is not null;
         }
         public override string ToString() => Convert.ToString(Name);
     }
