5239cb5491472322e5a9865005bd4e2e56939cf8
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-26 16:36:28 +0300

    Update Composite.cs

--- NETLab4/Tree/Composite.cs
+++ NETLab4/Tree/Composite.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace NETLab4.Tree
 {
     public class Composite : Component
     {
-        public override Component Parent { get; set; }
-        public override Component Left { get; set; }
-        public override Component Right { get; set; }
-        public override Connect Connector { get; set; }
-        public Composite() { }
+        public override Component? Parent { get; set; }
+        public override Component? Left { get; set; }
+        public override Component? Right { get; set; }
+        public override Connect? Connector { get; set; }
+        public Composite() { } 
 
         public Composite(Connect connector)
         {
@@ -22,9 +16,11 @@ namespace NETLab4.Tree
         public override string ToString()
         {
             string result = "(";
-            result += Left.ToString();
+            if(Left != null)
+                result += Left.ToString();
             result += Connector;
-            result += Right.ToString();
+            if(Right != null)
+                result += Right.ToString();
             return result + ")";
         }
     }
