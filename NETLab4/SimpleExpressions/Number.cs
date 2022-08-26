b9972d6679e70203012639e82c2cb4c31b10b513
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-08-26 16:34:54 +0300

    set -> init

--- NETLab4/SimpleExpressions/Number.cs
+++ NETLab4/SimpleExpressions/Number.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace NETLab4.SimpleExpressions
 {
     internal class Number : SimpleExpression
     {
-        public override double Value { get; set; }
+        public override double  Value { get; init; }
         public Number(double value)
         {
             Value = value;
